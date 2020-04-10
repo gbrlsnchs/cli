374402f771a875d1347053b32da363572632df97
Gabriel Sanches <gabriel@gsr.dev>
2020-04-09 21:00:11 -0300

    cli/clitest: add methods to get console outputs

--- clitest/program.go
+++ clitest/program.go
@@ -2,6 +2,7 @@ package clitest
 
 import (
 	"io"
+	"strings"
 
 	"github.com/gbrlsnchs/cli"
 )
@@ -11,21 +12,34 @@ var _ cli.Program = new(Program)
 
 // Program is a stub program that implements cli.Program.
 type Program struct {
-	name string
-	outw io.Writer
-	errw io.Writer
+	name   string
+	comb   *strings.Builder
+	out    *strings.Builder
+	errout *strings.Builder
 }
 
 // NewProgram returns a new stub program.
-func NewProgram(name string, outw, errw io.Writer) Program {
-	return Program{name, outw, errw}
+func NewProgram(name string) Program {
+	comb := new(strings.Builder)
+	out := new(strings.Builder)
+	errw := new(strings.Builder)
+	return Program{name, comb, out, errw}
 }
 
 // Name returns the program's name.
 func (p Program) Name() string { return p.name }
 
 // Stdout returns the program's stdout.
-func (p Program) Stdout() io.Writer { return p.outw }
+func (p Program) Stdout() io.Writer { return io.MultiWriter(p.out, p.comb) }
 
 // Stderr returns the program's stderr.
-func (p Program) Stderr() io.Writer { return p.errw }
+func (p Program) Stderr() io.Writer { return io.MultiWriter(p.errout, p.comb) }
+
+// Output returns what has been written to standard output.
+func (p Program) Output() string { return p.out.String() }
+
+// ErrOutput returns what has been written to standard error.
+func (p Program) ErrOutput() string { return p.errout.String() }
+
+// CombinedOutput returns what has been written to both standard output and error.
+func (p Program) CombinedOutput() string { return p.comb.String() }
