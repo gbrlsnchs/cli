2b2e3eb2a060e2ff015e448d7ad59cd0135a6362
Gabriel Sanches <gabriel@gsr.dev>
2020-04-13 19:03:04 -0300

    cli/cliutil: rename type for comma-separated list

--- cliutil/comma_sep_list_test.go
+++ cliutil/comma_sep_list_test.go
@@ -7,40 +7,40 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestCommaList(t *testing.T) {
+func TestCommaSepList(t *testing.T) {
 	t.Run("Set", func(t *testing.T) {
 		testCases := []struct {
 			input string
-			want  cliutil.CommaList
+			want  cliutil.CommaSepList
 		}{
-			{"", cliutil.CommaList{}},
-			{"foo", cliutil.CommaList{"foo"}},
-			{"foo,bar", cliutil.CommaList{"foo", "bar"}},
-			{"foo,bar,baz", cliutil.CommaList{"foo", "bar", "baz"}},
-			{"foo,,baz", cliutil.CommaList{"foo", "baz"}},
+			{"", cliutil.CommaSepList{}},
+			{"foo", cliutil.CommaSepList{"foo"}},
+			{"foo,bar", cliutil.CommaSepList{"foo", "bar"}},
+			{"foo,bar,baz", cliutil.CommaSepList{"foo", "bar", "baz"}},
+			{"foo,,baz", cliutil.CommaSepList{"foo", "baz"}},
 		}
 		for _, tc := range testCases {
 			t.Run("", func(t *testing.T) {
-				cs := make(cliutil.CommaList, 0)
+				cs := make(cliutil.CommaSepList, 0)
 				err := cs.Set(tc.input)
 				if want, got := (error)(nil), err; got != want {
 					t.Fatalf("want %v, got %v", want, got)
 				}
 				if want, got := tc.want, cs; !cmp.Equal(got, want) {
-					t.Fatalf("(*CommaList).Set mismatch (-want +got):\n%s", cmp.Diff(want, got))
+					t.Fatalf("(*CommaSepList).Set mismatch (-want +got):\n%s", cmp.Diff(want, got))
 				}
 			})
 		}
 	})
 	t.Run("String", func(t *testing.T) {
 		testCases := []struct {
-			cs   cliutil.CommaList
+			cs   cliutil.CommaSepList
 			want string
 		}{
-			{cliutil.CommaList{}, ""},
-			{cliutil.CommaList{"foo"}, "foo"},
-			{cliutil.CommaList{"foo", "bar"}, "foo,bar"},
-			{cliutil.CommaList{"foo", "bar", "baz"}, "foo,bar,baz"},
+			{cliutil.CommaSepList{}, ""},
+			{cliutil.CommaSepList{"foo"}, "foo"},
+			{cliutil.CommaSepList{"foo", "bar"}, "foo,bar"},
+			{cliutil.CommaSepList{"foo", "bar", "baz"}, "foo,bar,baz"},
 		}
 		for _, tc := range testCases {
 			t.Run("", func(t *testing.T) {
